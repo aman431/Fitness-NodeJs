2723e4783e46229de24763bea5c43db9c1d2a5ea
Aman Rathore <amanrathod777@gmail.com>
2020-09-11 21:08:31 +0530

    Updare

--- models/parq.js
+++ models/parq.js
@@ -35,6 +35,66 @@ const parqSchema = new mongoose.Schema({
 	weight:{
 		type:String,
 		required: [true,'This Field is required']
+	},
+	training_type:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	training_week:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	training_duration:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	cardio_type:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	cardio_week:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	cardio_duration:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	stretching_type:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	stretching_week:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	stretching_duration:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	sports_type:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	sports_week:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	sports_duration:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	other_type:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	other_week:{
+		type:String,
+		required: [true,'This Field is required']
+	},
+	other_duration:{
+		type:String,
+		required: [true,'This Field is required']
 	}
 });
 
